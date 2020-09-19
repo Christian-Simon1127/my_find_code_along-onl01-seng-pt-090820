3633c1efac38dc16271ed8204de410a38642121f
thoughtful-storage-6584 <csimon1127@gmail.com>
2020-09-19 22:06:40 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,7 +4,7 @@ def my_find(collection)
   counter = 0
   new_collection = []
   while counter < collection.length
-    yield(collection[counter])
+    if yield(collection[counter]).find
     counter += 1
   end
   new_collection
